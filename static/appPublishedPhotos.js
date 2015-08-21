fd2a4cc3a147b36ebaa5e21a109c14c142ec26ae
Cecil Westerhof <CLDWesterhof@gmail.com>
2015-08-21 22:01:17 +0200

    Added versioninfo for SQLite and Python

--- static/appPublishedPhotos.js
+++ static/appPublishedPhotos.js
@@ -4,24 +4,44 @@ angular.module('Published Photos', [])
             getLinks: function() {
                 return $http.get('/links/data').then(function(result) {
                     return result.data
-                });
-
+                })
             }
-        };
+        }
+        return service
+    })
+    .factory('versions', function($http) {
+        var service = {
+            getPythonVersion: function() {
+                return $http.get('/versionPython').then(function(result) {
+                    return result.data
+                })
+            },
+            getSQLiteVersion: function() {
+                return $http.get('/versionSQLite').then(function(result) {
+                    return result.data
+                })
+            }
+        }
         return service;
     })
-    .controller('PublishedPhotosController', function ($scope, $window, linksfetcher) {
+    .controller('PublishedPhotosController', function ($scope, $window, linksfetcher, versions) {
         $scope.title = 'Published Photos'
         linksfetcher.getLinks().then(function(links) {
-            $scope.links = links;
-        });
+            $scope.links = links
+        })
+        versions.getPythonVersion().then(function(versionPython) {
+            $scope.versionPython = versionPython
+        })
+        versions.getSQLiteVersion().then(function(versionSQLite) {
+            $scope.versionSQLite = versionSQLite
+        })
         $scope.openAll = function() {
             $scope.links.forEach(function(link) {
                 // console.log(link)
                 $window.open(link.url, "_blank")
             })
         }
-        $scope.getVersion = function() {
+        $scope.getAngularVersion = function() {
             return angular.version.full
         }
     })
